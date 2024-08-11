c75ec3533eeda357819130f135f5b425a33dce3f
coding-911 <ye22im@gmail.com>
2024-08-11 18:44:58 +0900

    Product API 기능 구현

--- product/src/main/java/com/sparta/msa_exam/product/ProductController.java
+++ product/src/main/java/com/sparta/msa_exam/product/ProductController.java
@@ -1,13 +1,20 @@
 package com.sparta.msa_exam.product;
 
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("/product")
 public class ProductController {
+
+    private final ProductService productService;
 
     @Value("${server.port}") // 애플리케이션이 실행 중인 포트를 주입받습니다.
     private String serverPort;
@@ -17,8 +24,15 @@ public class ProductController {
         response.setHeader("Server-Port", serverPort);
     }
 
-    @GetMapping("/product")
-    public String getProduct() {
-        return "Product info!!!!! From port : " + serverPort;
+    @PostMapping()
+    public Product createProduct(@RequestBody Product product) {
+        // 응답 보내기
+        return productService.createProduct(product);
+    }
+
+    @GetMapping()
+    public List<Product> getAllProducts() {
+        // 응답 보내기
+        return productService.getAllProducts();
     }
 }
