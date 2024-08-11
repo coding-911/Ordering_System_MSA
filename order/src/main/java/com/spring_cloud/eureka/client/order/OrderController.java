888984a3e3fe5fb53c29dbb146f4ca86f27b3964
coding-911 <ye22im@gmail.com>
2024-08-11 16:17:27 +0900

    Response Header에 "Server-Port": 포트번호 추가

--- order/src/main/java/com/spring_cloud/eureka/client/order/OrderController.java
+++ order/src/main/java/com/spring_cloud/eureka/client/order/OrderController.java
@@ -1,14 +1,27 @@
 package com.spring_cloud.eureka.client.order;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/order")
 public class OrderController {
 
+    @Value("${server.port}") // 애플리케이션이 실행 중인 포트를 주입받습니다.
+    private String serverPort;
+
+    @ModelAttribute
+    public void setResponseHeader(HttpServletResponse response) {
+        response.setHeader("Server-Port", serverPort);
+    }
+
     @GetMapping
     public String getOrder() {
-        return "Order details";
+        return "Order details"+ serverPort;
     }
 }
--- product/src/main/java/com/sparta/msa_exam/product/ProductController.java
+++ product/src/main/java/com/sparta/msa_exam/product/ProductController.java
@@ -1,8 +1,9 @@
 package com.sparta.msa_exam.product;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,11 @@ public class ProductController {
 
     @Value("${server.port}") // 애플리케이션이 실행 중인 포트를 주입받습니다.
     private String serverPort;
+
+    @ModelAttribute
+    public void setResponseHeader(HttpServletResponse response) {
+        response.setHeader("Server-Port", serverPort);
+    }
 
     @GetMapping("/product")
     public String getProduct() {
